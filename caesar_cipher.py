cc1911c58a6fbf3c8945b92ef71fb8149127e880
PillalamariV <107156925+PillalamariV@users.noreply.github.com>
2025-06-13 11:36:31 -0500

    make code simplified

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,56 +1,32 @@
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
-            "u", "v", "w", "x", "y", "z"]
-
-start = input("Type 'decrypt' to decode, 'encrypt' to encrypt: ").lower()
-text = input("Enter text: ").lower()
-shift = int(input("Enter shift: "))
-
-def encrypt(text, shift):
-    message = []
-    for x in text:
-        if x in alphabet:
-            #range(inclusive, exclusive)
-            for i in range(0, len(alphabet)):
-                if (x == alphabet[i]):
-                    #modulo has presedence (shift to right)
-                    message.append(alphabet[(i + shift) % (len(alphabet))])
+def encryption(message, shift):
+    new_message = ""
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    symbols = list("!@#$%^&*()1234567890")
+    for x in message.lower():
+        if (x != " " and not(x in symbols)):
+            new_index = (alphabet.index(x) + shift) % 26
+            new_message += alphabet[new_index]
         else:
-            message.append(x)
-    encrypted = "".join(message)
-    print(f"Encrypted Message: {encrypted}")
-  
-def decrypt(text, shift):
-    message = []
-    for x in text:
-        if x in alphabet:
-            for i in range(0, len(alphabet)):
-                if (x == alphabet[i]):
-                    #modulo by length of alphabet because otherwise at 25, z would be considered a since (25%25 = 0)
-                    message.append(alphabet[(i - shift) % (len(alphabet))])
+            new_message += x
+    print(new_message)
 
+def decryption(message, shift):
+    old_message = ""
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    symbols = list("!@#$%^&*()1234567890")
+    for x in message.lower():
+        if (x != " " and not(x in symbols)):
+            old_index = (alphabet.index(x) - shift) % 26
+            old_message += alphabet[old_index]
         else:
-            message.append(x)
-    decrypted = "".join(message)
-    print(f"The message is: {decrypted}")
+            old_message += x
+    print(old_message)
+decryption("wtaad ldgas!", 15)
 
-user = True
-while (user):
-    while start != "encrypt" and start != "decrypt":
-        print("Please type exactly")
-        start = input("Type 'decrypt' to decode, 'encrypt' to encrypt: ").lower()
-    if start == "decrypt":
-        decrypt(text, shift)
-    elif start == "encrypt":
-        encrypt(text, shift)
-    cipher = input("\nType 'yes' if you want to go again, otherwise type 'no': ")
-    if cipher == "no":
-        user = False
-    else:
-        start = input("\nType 'decrypt' to decode, 'encrypt' to encrypt: ").lower()
-        while start != "encrypt" and start != "decrypt":
-            print("Please type exactly")
-            start = input("Type 'decrypt' to decode, 'encrypt' to encrypt: ").lower()
-        text = input("Enter text: ").lower()
-        shift = int(input("Enter shift: "))
-
+choice = input("Type 'encode' to encrypt, type'decode' to decrypt: ")
+message = input("Type the message: ")
+shift = int(input("Type shift number: "))
+if (choice == "encode"):
+    encryption(message, shift)
+else:
+    decryption(message, shift)
